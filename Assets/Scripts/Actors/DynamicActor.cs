2663a9aa4e9569e7d9444363ded975dc04bbe8c7
José Villegas <villegasjose.gg@gmail.com>
2016-09-01 08:33:38 -0400

    movement and definitions

---
+++ Assets/Scripts/Actors/DynamicActor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Dynamic Actor", order = 1)]
+public class DynamicActor : ScriptableObject
+{
+    [SerializeField]
+    private float _horizontalForce = 5.0f;
+    [SerializeField]
+    private float _verticalForce = 50.0f;
+    [SerializeField]
+    private float _distanceToGround = 0.1f;
+
+    public float HorizontalForce
+    {
+        get
+        {
+            return _horizontalForce;
+        }
+    }
+
+    public float VerticalForce
+    {
+        get
+        {
+            return _verticalForce;
+        }
+    }
+
+    public float DistanceToGround
+    {
+        get
+        { 
+            return _distanceToGround;
+        }
+    }
+}
--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,15 +1,76 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private DynamicActor _player;
+    private Rigidbody _rigidbody;
+    private Collider _collider;
+    private Vector3 _movementVector = Vector3.zero;
+    private bool _onJump;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void Start()
+    {
+        // player's rigidbody for movement
+        _rigidbody = GetComponent<Rigidbody>();  
+
+        if (!_rigidbody)
+        {
+            Debug.LogError("Couldn't obtain Rigidbody component in " + this);
+            enabled = false;
+        }
+
+        _collider = GetComponent<Collider>();
+
+        if (!_collider)
+        {
+            Debug.LogError("Couldn't obtain Collider component in " + this);
+            enabled = false;
+        }
+
+        if (!_player)
+        {
+            Debug.LogError("No DynamicActor provided");
+            enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// Determines whether the player is on the ground, on top of a collider
+    /// </summary>
+    /// <returns><c>true</c> if the player is on top of a collider; otherwise, <c>false</c>.</returns>
+    private bool IsGrounded()
+    {
+        float extend = _collider.bounds.extents.y;
+        return Physics.Raycast(transform.position, -Vector3.up, extend + _player.DistanceToGround);
+    }
+
+    private void FixedUpdate()
+    {
+        bool grounded = IsGrounded();
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+        _movementVector = Vector3.zero;
+        // horizontal movement - forward and backwards, check if player 
+        // is grounded to avoid air strafing
+        if (grounded)
+        {
+            _movementVector.x = Input.GetAxis("Horizontal") * _player.HorizontalForce;
+        }
+
+        // jumping only when the character is on the ground
+        if (verticalAxis > 0.0f && grounded && !_onJump)
+        {
+            _movementVector.y = _player.VerticalForce;
+            _onJump = true;
+        }
+
+        // reset on jump once the player is on the ground
+        if (grounded && _movementVector.y == 0.0f)
+        {
+            _onJump = false;
+        }
+
+        _rigidbody.AddForce(_movementVector);
+    }
 }
